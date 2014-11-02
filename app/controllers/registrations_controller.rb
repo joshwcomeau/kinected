67e70771295c75a9e401e89884d5b4a4e6a9807d
Josh Comeau <joshwcomeau@gmail.com>
2014-11-02 12:46:10 -0800

    Created a registrations controller, inherited from Devise::Registrations. Added custom strong param attributes.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < Devise::RegistrationsController
+ 
+  private
+ 
+  def sign_up_params
+    params.require(:user).permit(
+      :email, :password, :password_confirmation, :birthdate, :country, :postal_code, :sex, :self_summary, :height, :income,
+      :num_of_kids, :body_type, :smoking, :drinking, :religion, :education, :work_industry, :wants_kids, :relationship_status
+    )
+  end
+
+  # def account_update_params
+  #   params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  # end
+end
