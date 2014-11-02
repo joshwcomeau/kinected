893ede49c65ff75181a2b5fd14f5064eecce7cc3
Josh Comeau <joshwcomeau@gmail.com>
2014-11-02 13:26:02 -0800

    Successfully saving ethnicities painlessly. Added ethnicities to seeds

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,8 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(
       :email, :password, :password_confirmation, :birthdate, :country, :postal_code, :sex, :self_summary, :height, :income,
-      :num_of_kids, :body_type, :smoking, :drinking, :religion, :education, :work_industry, :wants_kids, :relationship_status
+      :num_of_kids, :body_type, :smoking, :drinking, :religion, :education, :work_industry, :wants_kids, :relationship_status,
+      ethnicity_ids: []
     )
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Destroy everything
+
+if Rails.env.production?
+  User.destroy_all
+  Ethnicity.destroy_all
+end
+
+Ethnicity.create([
+  { name: 'African / Black' },
+  { name: 'Asian' },
+  { name: 'Latino' },
+  { name: 'Indian' },
+  { name: 'White' },
+])
