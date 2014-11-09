34b01346fcaacaf33a64fc397f721616ee1722cd
Joshua Comeau <joshwcomeau@gmail.com>
2014-11-09 13:43:16 -0500

    Working Angular form validation in controller. woot.

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -3,5 +3,15 @@ var app = angular.module('kinected', []);
 app.controller('UserRegistrationController', UserRegistrationController);
 
 function UserRegistrationController() {
-  
+  this.current_step = 1;
+  this.step_1_submitted = false;
+}
+
+UserRegistrationController.prototype.goToStep2 = function() {
+  // Fields to check are first_name, last_name, birthdate fields and sex.
+  console.log(this.myForm['user[first_name]'].$pristine);
+}
+
+UserRegistrationController.prototype.setForm = function(form) {
+  this.myForm = form
 }
