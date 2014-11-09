3714e416dc250cbbcefb2e549bca0618fa2f4ea1
Joshua Comeau <joshwcomeau@gmail.com>
2014-11-09 17:09:07 -0500

    Refactored the Angular logic, so that now it's generalized for any step, not a specific function per step.

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -4,14 +4,67 @@ app.controller('UserRegistrationController', UserRegistrationController);
 
 function UserRegistrationController() {
   this.current_step = 1;
-  this.step_1_submitted = false;
+  this.num_of_steps = 2;
+  this.state_info = {
+    step1: {
+      index: 1,
+      required_fields: [
+        'user[first_name]', 'birthdate_month',
+        'user[last_name]',  'birthdate_day',
+        'user[sex]',        'birthdate_year'
+      ]
+    },
+    step2: {
+      index: 2,
+      required_fields: [
+        'user[email]', 'user[password]', 'user[password_confirmation]'
+      ]
+    }
+  }
+  this.state_status = {
+    step1: {
+      submitted:  false,
+      valid:      true
+    },
+    step2: {
+      submitted:  false,
+      valid:      true
+    }
+  }
 }
 
-UserRegistrationController.prototype.goToStep2 = function() {
-  // Fields to check are first_name, last_name, birthdate fields and sex.
-  console.log(this.myForm['user[first_name]'].$pristine);
-}
+UserRegistrationController.prototype.validateStep = function(step_num, e) {
+  var step = "step"+step_num;
 
-UserRegistrationController.prototype.setForm = function(form) {
-  this.myForm = form
+  // Reset our validation flag. We can't use Angular's built-in one, because we're using
+  // a multi-stage form. We're not actually submitting the form, just a fieldset.
+  this.state_status[step].valid = true;
+
+
+  // In the HTML, error messages are shown if step_1_submitted is true, and step_1_valid is false.
+  checkRequiredFields(this.state_info[step].required_fields, step, this);
+
+  // Mark this form as having been submitted
+  this.state_status[step].submitted = true;
+
+  // Is this the final step?
+  if ( this.current_step === this.num_of_steps ) {
+    // If it's invalid, prevent the default (submitting) behavior.
+    if ( this.registration_form.$invalid ) { e.preventDefault(); }
+  } else {
+    // If this step is valid, let's progress to the next.
+    if ( this.state_status[step].valid ) {
+      this.current_step++;
+    }
+  }
+
+};
+
+function checkRequiredFields(fields, step, context) {
+  _.each(fields, function(element, index, list) {
+    if ( context.registration_form[element].$invalid ) {
+      context.state_status[step].valid = false;
+    }
+  });
+
 }
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
   
-  # GET /resource/sign_up
+  # GET /users/sign_up
   def new
     build_resource({})
     @validatable = devise_mapping.validatable?
     if @validatable
       @minimum_password_length = resource_class.password_length.min
     end
-    # All of the above is straight-up copied from the Devise::Registrations controller. We're just changing what renders.
+    # All of the above is straight-up copied from the Devise::Registrations controller. 
     respond_with self.resource, layout: 'onboarding'    
   end
 
-  # POST /resource
+  # POST /users
   def create
     build_resource(sign_up_params)
+
+    # Let's grab the 3 date fields from the params, use it to create a Date, and attach it to resource.
+    date_string = "#{params[:birthdate_year]}-#{params[:birthdate_month]}-#{params[:birthdate_day]}"
+    date_obj    = Date.strptime(date_string, "%Y-%m-%d")
+    resource.birthdate = date_obj
 
     resource_saved = resource.save
     yield resource if block_given?
@@ -21,11 +26,11 @@ class RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        respond_with resource, location: after_sign_up_path_for(resource), layout: 'onboarding' 
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        respond_with resource, location: after_inactive_sign_up_path_for(resource), layout: 'onboarding' 
       end
     else
       clean_up_passwords resource
@@ -33,7 +38,7 @@ class RegistrationsController < Devise::RegistrationsController
       if @validatable
         @minimum_password_length = resource_class.password_length.min
       end
-      respond_with resource, layout: false
+    respond_with self.resource, layout: 'onboarding'    
     end
   end
 
@@ -43,7 +48,7 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(
       :email, :password, :password_confirmation, :birthdate, :country, :postal_code, :sex, :self_summary, :height, :income,
       :num_of_kids, :body_type, :smoking, :drinking, :religion, :education, :work_industry, :wants_kids, :relationship_status,
-      ethnicity_ids: []
+      :first_name, :last_name, ethnicity_ids: []
     )
   end
 
