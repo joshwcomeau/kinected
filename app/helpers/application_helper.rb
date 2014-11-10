4b85dfae65af82d3d35fdd0bb6c77643147e4c23
Joshua Comeau <joshwcomeau@gmail.com>
2014-11-10 17:13:23 -0500

    Implemented i18n, started transferring copy on the registration page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def fetch_content(key, format=false)
+    @string = "#{params[:controller]}.#{params[:action]}.#{key.to_s}"
+    format == :raw ? raw(t(@string)) : t(@string)
+  end
 end
