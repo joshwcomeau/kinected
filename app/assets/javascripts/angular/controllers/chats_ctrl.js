5f89126c42bda1c69770e8b4c495e6224210018c
Josh Comeau <joshwcomeau@gmail.com>
2014-11-13 14:38:16 -0500

    Couple chat tweaks. text input clears after send. Images are floated.

--- app/assets/javascripts/angular/controllers/chats_ctrl.js
+++ app/assets/javascripts/angular/controllers/chats_ctrl.js
@@ -1,6 +1,7 @@
 function ChatsController($scope, $attrs, $firebase) {
-  this.sender   = $attrs.sender;
-  this.receiver = $attrs.receiver;
+  this.sender     = $attrs.sender;
+  this.receiver   = $attrs.receiver;
+  this.thumbnail  = $attrs.thumbnail; 
 
   var ref = new Firebase("https://kinected.firebaseio.com/");
   var sync = $firebase(ref);
@@ -15,9 +16,12 @@ ChatsController.prototype.pushMessage = function() {
   this.messages.$add({
     sender:     this.sender,
     receiver:   this.receiver,
+    thumbnail:  this.thumbnail,
     message:    this.current_input,
     created_at: Firebase.ServerValue.TIMESTAMP
   });
+  // Clear the input
+  this.current_input = '';
 };
 
 ChatsController.prototype.fromSelf = function(message) { return message.sender == this.sender; };
