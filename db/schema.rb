7676f181d975ef4463f49d18f5f497664b7d8353
Joshua Comeau <joshwcomeau@gmail.com>
2014-11-16 07:48:04 -0500

    Added city/state to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115234941) do
+ActiveRecord::Schema.define(version: 20141116124435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,8 @@ ActiveRecord::Schema.define(version: 20141115234941) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "display_name"
+    t.string   "city"
+    t.string   "state"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
