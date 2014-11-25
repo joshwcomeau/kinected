68963e7154649d32df5bf19f0dba0e6ad85d0f7d
Joshua Comeau <joshwcomeau@gmail.com>
2014-11-25 08:17:12 -0500

    Quick, tiny commit, so I can work on another computer. Removed unnecessary line from OnlineCtrl

--- app/assets/javascripts/angular/controllers/online_ctrl.js
+++ app/assets/javascripts/angular/controllers/online_ctrl.js
@@ -9,12 +9,8 @@ function OnlineController($scope, $attrs, $firebase) {
   updated_info[this.userId] = this.curTime
 
   sync.$update(updated_info);
-
-
 }
 
-
-OnlineController.prototype.fromSelf = function(message) { return message.sender == this.sender; };
 
 OnlineController.$inject = ['$scope', '$attrs', '$firebase'];
 app.controller('OnlineController', ['$scope', '$attrs', '$firebase', OnlineController]);
